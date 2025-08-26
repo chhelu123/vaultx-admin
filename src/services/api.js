beefc487afb0ad2a781695ee70167489c86aba03
chhelu123 <chhelurathore773@gmail.com>
2025-08-27 03:24:56 +0530

    Update API URL for production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api/admin',
+  baseURL: 'https://vaultx-backend-production.up.railway.app/api/admin',
 });
 
 api.interceptors.request.use((config) => {
